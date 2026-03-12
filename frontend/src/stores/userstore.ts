9f377fe4faf7c39f81ac619386f54114a4cdd3a6
flwergg <gabymartinez12319@gmail.com>
2026-03-11 23:59:09 -0500

    feat: create AuthService and move auth logic from UserService

--- frontend/src/stores/userstore.ts
+++ frontend/src/stores/userstore.ts
@@ -1,9 +1,11 @@
+// Author: Gaby
 import type { UserInterface } from '@/interfaces/UserInterface.js';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useUserStore = defineStore('user', () => {
   const users = ref<UserInterface[]>([]);
+  const loggedUser = ref<UserInterface | null>(null);
 
-  return { users };
+  return { users, loggedUser };
 });
